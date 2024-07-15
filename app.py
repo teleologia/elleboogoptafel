80f338af99b783575cfe470fd1285250f4dfb031
installer <installer@gmail.com>
2024-07-16 00:43:50 +0200

    made entry more mobile centric

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/bereken_scores', methods=['POST'])
 def bereken_scores():
@@ -24,6 +26,7 @@ def bereken_scores():
         scores.append(score)
 
     return jsonify(scores=scores)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
